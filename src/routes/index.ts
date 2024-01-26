ab1c5b0e5911e5bd45a139837ebc80b38c456fe8
Ataias Pereira Reis <ataiasreis@gmail.com>
2024-01-26 08:58:39 -0300

    Mark unused variables in handlers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 /* GET home page. */
-router.get("/", (req, res, next) => {
+router.get("/", (_req, res, _next) => {
   res.render("index", { title: "Express" });
 });
 
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 /* GET users listing. */
-router.get("/", (req, res, next) => {
+router.get("/", (_req, res, _next) => {
   res.send("respond with a resource");
 });
 
